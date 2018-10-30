98a33df741a939f6a824f06169e1aca8b0d16800
Dirk Newbold <32912390+dirknewbold@users.noreply.github.com>
2018-10-30 10:56:57 +1000

    EntryMixin: Added mixin for views where site authentication is a prerequisite.

---
+++ README.md
@@ -0,0 +1,41 @@
+
+# Django Meetup
+
+## Project Overview
+An open source project providing a borderless network for the Django curious,
+newbie and professional to share experiences in Python-Django web development.
+This is the master repository for https://djangomeetup.com
+
+### Stretch Goal
+To achieve similar daily active users as https://www.djangoproject.com/ with all
+code available within this repository.
+
+## Contacts
+Email: info@djangomeetup.com
+Telegram: https://t.me/DjangoMeetup
+
+## Apps Overview
+Following are brief descriptions of the apps developed for this project.
+
+### Anonymous
+Application handling the web pages associated with an anonymous user.
+
+### Formality
+Application handling additional form functionality and bootstrap rendering.
+
+### Glaze
+Application implementing bootstrap's modal dialog and ajax handling of form
+submission within such a dialog.
+https://getbootstrap.com/docs/4.0/components/modal/
+
+## Reference Material
+
+### Official Documentation
+https://www.djangoproject.com/
+
+### Courses Of Note
+https://www.udemy.com/python-and-django-full-stack-web-developer-bootcamp/learn/v4/content
+https://www.udacity.com/course/how-to-use-git-and-github--ud775
+
+### General Resources
+https://simpleisbetterthancomplex.com/archive/
--- apps/anonymous/views.py
+++ apps/anonymous/views.py
@@ -2,6 +2,7 @@
 from apps.anonymous.forms import EntryForm
 from apps.formality.views import evaluate_recaptcha
 from apps.glaze.views import GlazeMixin
+from public_website import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.urls import reverse, reverse_lazy
@@ -18,6 +19,10 @@ class ErrorGlaze(GlazeMixin, TemplateView):
 class HoldingView(TemplateView):
 
     template_name = "anonymous/holding.html"
+
+    def dispatch(self, request, *args, **kwargs):
+        request.session["is_site_accessible"] = False
+        return super().dispatch(request, *args, **kwargs)
 
 
 class EntryGlaze(GlazeMixin, FormView):
@@ -37,11 +42,27 @@ class EntryGlaze(GlazeMixin, FormView):
 
 
 def entry_granted(request):
-
     request.session["is_site_accessible"] = True
     return HttpResponseRedirect(reverse("anonymous:home"))
 
 
-class HomeView(TemplateView):
+def is_site_accessible(request):
+    # Answer true if the session has access to DjangoMeetup main website
+    if not ("is_site_accessible" in request.session):
+        request.session["is_site_accessible"] = False
+    return (settings.DEBUG or request.session["is_site_accessible"])
+
+
+class EntryMixin:
+    # Mixin class for view classes to ensure session has access
+    def dispatch(self, request, *args, **kwargs):
+        if is_site_accessible(request):
+            return super().dispatch(request, *args, **kwargs)
+        else:
+            request.session["glaze_url"] = reverse("anonymous:entry")
+            return redirect("anonymous:holding")
+
+
+class HomeView(EntryMixin, TemplateView):
 
     template_name = "anonymous/home.html"
