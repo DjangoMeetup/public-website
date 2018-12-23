45383f500b72ef8c4456180942dc6e26e8dae652
Felix <felix.p.hall@gmail.com>
2018-12-23 11:42:44 +1000

    git problems

--- public_website/apps/anonymous/urls.py
+++ public_website/apps/anonymous/urls.py
@@ -1,9 +1,13 @@
 from public_website.apps.anonymous import views
 from django.urls import path
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> upstream/master
+=======
+
+>>>>>>> 8ef495962d88aa4d98ce223d1ca1206eafaf114b
 
 # Template tagging Django reference
 app_name = 'anonymous'
@@ -17,9 +21,15 @@ urlpatterns = [
     path('home/', views.HomeView.as_view(), name='home'),
     path('login_required/', views.LoginRequiredGlaze.as_view(), name='login_required'),
 <<<<<<< HEAD
+<<<<<<< HEAD
     path('/', views.HoldingView.as_view(), name='holding'),
 =======
     path('', views.HoldingView.as_view(), name='holding'),
 >>>>>>> upstream/master
+=======
+
+    path('', views.HoldingView.as_view(), name='holding'),
+
+>>>>>>> 8ef495962d88aa4d98ce223d1ca1206eafaf114b
 ]
 
--- public_website/apps/member/urls.py
+++ public_website/apps/member/urls.py
@@ -1,8 +1,13 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 from django.urls import path, re_path
 =======
 from django.urls import path
 >>>>>>> upstream/master
+=======
+from django.urls import path, re_path
+
+>>>>>>> 8ef495962d88aa4d98ce223d1ca1206eafaf114b
 
 from member import views
 
@@ -18,16 +23,24 @@ urlpatterns = [
         name='password_reset_acknowledged'),
     path('password_reset_complete/', views.PasswordResetCompleteGlaze.as_view(), name='password_reset_complete'),
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> 8ef495962d88aa4d98ce223d1ca1206eafaf114b
     re_path(r'^password_reset_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$/',
         views.PasswordResetConfirmGlaze.as_view(), name='password_reset_confirm'),
     path('password_reset_failed/', views.PasswordResetFailedGlaze.as_view(), name='password_reset_failed'),
     re_path(r'^password_reset_verification/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$/',
+<<<<<<< HEAD
 =======
     path('password_reset_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
         views.PasswordResetConfirmGlaze.as_view(), name='password_reset_confirm'),
     path('password_reset_failed/', views.PasswordResetFailedGlaze.as_view(), name='password_reset_failed'),
     path('password_reset_verification/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
 >>>>>>> upstream/master
+=======
+
+>>>>>>> 8ef495962d88aa4d98ce223d1ca1206eafaf114b
         views.password_reset_verification, name='password_reset_verification'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('signup/', views.SignupGlaze.as_view(), name='signup'),
@@ -36,10 +49,15 @@ urlpatterns = [
     path('signup_verified_previously/', views.SignUpVerifiedPreviouslyGlaze.as_view(),
         name='signup_verified_previously'),
 <<<<<<< HEAD
+<<<<<<< HEAD
     re_path(r'^signup_verification/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$/',
 =======
     path('signup_verification/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
 >>>>>>> upstream/master
+=======
+
+    re_path(r'^signup_verification/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$/',
+>>>>>>> 8ef495962d88aa4d98ce223d1ca1206eafaf114b
         views.signup_verification, name='signup_verification'),
     path('signup_welcome/', views.SignUpWelcomeGlaze.as_view(), name='signup_welcome'),
 ]
