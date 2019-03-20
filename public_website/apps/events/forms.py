eb34a1acfdf9f2e811967602893b7ae17fb0e0a1
Felix <felix.p.hall@gmail.com>
2019-03-20 20:14:51 +1000

    changed indentation from tabs to spaces.

--- public_website/apps/events/forms.py
+++ public_website/apps/events/forms.py
@@ -5,75 +5,75 @@ from formality.widgets import XDSoftDateTimePickerInput
 
 
 class GroupCreationForm(forms.ModelForm):
-	
-	class Meta:
-		model = EventGroup
-		fields = ('name', 'details')
+    
+    class Meta:
+        model = EventGroup
+        fields = ('name', 'details')
 
-		widgets = {
-		'details': forms.Textarea,	
-		}
+        widgets = {
+        'details': forms.Textarea,  
+        }
 
 class GroupRegisterForm(forms.Form):
-	groups = forms.ModelMultipleChoiceField(queryset=EventGroup.objects.all())
+    groups = forms.ModelMultipleChoiceField(queryset=EventGroup.objects.all())
 
 
 
 class EventCreationForm(forms.ModelForm):
 
-	STATE_CHOICES = (
-		('ACT', 'Australian Capital Territory'),
-		('NSW', 'New South Wales'),
-		('NT', 'Northern Territory'),
-		('QLD', 'Queensland'),
-		('SA', 'South Australia'),
-		('TAS', 'Tasmania'),
-		('VIC', 'Victoria'),
-		('WA', 'Western Australia')
-		)
+    STATE_CHOICES = (
+        ('ACT', 'Australian Capital Territory'),
+        ('NSW', 'New South Wales'),
+        ('NT', 'Northern Territory'),
+        ('QLD', 'Queensland'),
+        ('SA', 'South Australia'),
+        ('TAS', 'Tasmania'),
+        ('VIC', 'Victoria'),
+        ('WA', 'Western Australia')
+        )
 
-	address_1 = forms.CharField(
-		label = 'Address',
-		required=True,
-		)
-	suburb = forms.CharField(
-		label = 'City/Suburb',
-		required=False)
-	postcode = forms.IntegerField(
-		label = 'Postcode',
-		required=True,
-		max_value=9999,
-		min_value=0,
-		)
+    address_1 = forms.CharField(
+        label = 'Address',
+        required=True,
+        )
+    suburb = forms.CharField(
+        label = 'City/Suburb',
+        required=False)
+    postcode = forms.IntegerField(
+        label = 'Postcode',
+        required=True,
+        max_value=9999,
+        min_value=0,
+        )
 
-	state = forms.ChoiceField(
-		label = 'State',
-		required=True,
-		choices=STATE_CHOICES
-		)
+    state = forms.ChoiceField(
+        label = 'State',
+        required=True,
+        choices=STATE_CHOICES
+        )
 
 
-	class Meta:
-		model = Events
-		fields = ('name', 'details', 'image', 'day', 'group')
+    class Meta:
+        model = Events
+        fields = ('name', 'details', 'image', 'day', 'group')
 
-		widgets = {
-		'day': XDSoftDateTimePickerInput(),
-		'details': forms.Textarea,		
-		}
-		input_formats = {
-		'day': ['%d/%m/%Y %H:%M'],
-		}
-	
-	def __init__(self, *args, **kwargs):
-		user = kwargs.pop('user', None)
+        widgets = {
+        'day': XDSoftDateTimePickerInput(),
+        'details': forms.Textarea,      
+        }
+        input_formats = {
+        'day': ['%d/%m/%Y %H:%M'],
+        }
+    
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
 
-		super(EventCreationForm, self).__init__(*args, **kwargs)
-		#if the user is authenticated
-		if user:
-			#print ('form: authenticated')
-			self.fields['group'].queryset = EventGroup.objects.filter(main_user_group=user)
-		#otherwise just an empty queryset
-		else:
-			#print ('form: not authenticated')
-			self.fields['group'].queryset = EventGroup.objects.none()
+        super(EventCreationForm, self).__init__(*args, **kwargs)
+        #if the user is authenticated
+        if user:
+            #print ('form: authenticated')
+            self.fields['group'].queryset = EventGroup.objects.filter(main_user_group=user)
+        #otherwise just an empty queryset
+        else:
+            #print ('form: not authenticated')
+            self.fields['group'].queryset = EventGroup.objects.none()
--- public_website/apps/events/models.py
+++ public_website/apps/events/models.py
@@ -9,55 +9,55 @@ from member.models import Person
 
 
 class UserEventGroupManager(models.Manager):
-	def get_queryset(self, request):
-		queryset = EventGroup.objects.filter(main_user_group=request.user)
-		return queryset
+    def get_queryset(self, request):
+        queryset = EventGroup.objects.filter(main_user_group=request.user)
+        return queryset
 
 class EventGroup(Group):
 
-	details = models.CharField(max_length=1500, blank=True, null=True)
+    details = models.CharField(max_length=1500, blank=True, null=True)
 
-	organisor_group = models.ManyToManyField(Person, 
-		related_name='group_organisor', default='None', blank=True)
-	main_user_group = models.ManyToManyField(Person, 
-		related_name='group_user', default='None', blank=True)
+    organisor_group = models.ManyToManyField(Person, 
+        related_name='group_organisor', default='None', blank=True)
+    main_user_group = models.ManyToManyField(Person, 
+        related_name='group_user', default='None', blank=True)
 '''
 class EventGroupOld(models.Model):
-	name = models.CharField(max_length=200, blank=True, default='Event Group', unique=True)
-	details = models.CharField(max_length=1500, blank=True, null=True)
+    name = models.CharField(max_length=200, blank=True, default='Event Group', unique=True)
+    details = models.CharField(max_length=1500, blank=True, null=True)
 
-	organisor_group = models.ManyToManyField(Person, 
-		related_name='group_organisor_old', default='None', blank=True)
-	main_user_group = models.ManyToManyField(Person, 
-		related_name='group_user_old', default='None', blank=True)
+    organisor_group = models.ManyToManyField(Person, 
+        related_name='group_organisor_old', default='None', blank=True)
+    main_user_group = models.ManyToManyField(Person, 
+        related_name='group_user_old', default='None', blank=True)
 
-	class Meta:
-		verbose_name_plural = 'events'
+    class Meta:
+        verbose_name_plural = 'events'
 
-	def __str__(self):
-		return '{}'.format(self.name)
+    def __str__(self):
+        return '{}'.format(self.name)
 '''
 class Events(models.Model):
-	name = models.CharField(max_length=200, blank=True, 
-		default='Unnamed Event')
-	day = models.DateTimeField(default=now, blank=True )
-	address = models.CharField(max_length=250, blank=False, 
-		default='', null=True)
-	image = models.ImageField(blank=True, null=True)
-	details = models.CharField(max_length=1500, blank=True, null=True)
-	slug = models.SlugField(max_length=255, default=name)
+    name = models.CharField(max_length=200, blank=True, 
+        default='Unnamed Event')
+    day = models.DateTimeField(default=now, blank=True )
+    address = models.CharField(max_length=250, blank=False, 
+        default='', null=True)
+    image = models.ImageField(blank=True, null=True)
+    details = models.CharField(max_length=1500, blank=True, null=True)
+    slug = models.SlugField(max_length=255, default=name)
 
-	group = models.ForeignKey(EventGroup, on_delete=models.CASCADE, null=True)
+    group = models.ForeignKey(EventGroup, on_delete=models.CASCADE, null=True)
 
-	attendees = models.ManyToManyField(Person, related_name='attendees', default='None', blank=True)
-	organisor = models.ForeignKey(Person, on_delete=models.CASCADE, related_name = 'event_organisor', null=True)
+    attendees = models.ManyToManyField(Person, related_name='attendees', default='None', blank=True)
+    organisor = models.ForeignKey(Person, on_delete=models.CASCADE, related_name = 'event_organisor', null=True)
 
-	class Meta:
-		verbose_name_plural = 'events'
+    class Meta:
+        verbose_name_plural = 'events'
 
-	def __str__(self):
-		return '{}'.format(self.name)
-	
-	def save(self, *args, **kwargs):
-		self.slug = slugify(self.name)
-		super(Events, self).save(*args, **kwargs)
+    def __str__(self):
+        return '{}'.format(self.name)
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Events, self).save(*args, **kwargs)
